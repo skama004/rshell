67dc98e287107ac0f7b5360d48f1bc88d2f950de
Siddaardha Kamarsu <skama004@hammer.cs.ucr.edu>
2016-11-02 19:10:31 -0700

    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,11 @@
 using namespace std;
 
 void exec(string input, int& status);
+//Declared function here so we can use it in run 
+//Actual implentation further below
 
 
-
-void run(vector<string> &cmd, vector<int> &cnct){
+void run(vector<string> &cmd, vector<int> &cnct){ //Function used to actually run
      unsigned int i = 0;
      int j;
      string str = cmd.at(0);
@@ -39,11 +40,11 @@ void run(vector<string> &cmd, vector<int> &cnct){
 
 }
 
-void getCommands(string input, vector<string> &v){
+void getCommands(string input, vector<string> &v){ //Function that takes commands and places in array 
      int pos = 0;
      string temp = " ";
      for(unsigned int i = 0; i < input.size(); i++){
-         if(input.at(i) == '&' || input.at(i) == '|'){
+         if(input.at(i) == '&' || input.at(i) == '|'){ //Tests if a connector is found
              temp = input.substr(pos, i - pos);
              v.push_back(temp);
              pos = i + 2;
@@ -66,26 +67,26 @@ void getCommands(string input, vector<string> &v){
 }
 
 
-bool getConnectors(const string &input, vector<int> & v){
+bool getConnectors(const string &input, vector<int> & v){ //Function that gets connectors from user string and places in array with numerical vals
      for(unsigned int i = 0; i < input.size(); i++){
-        if(input.at(i) == ';'){
+        if(input.at(i) == ';'){ //Test for semicolons
            i++;
            if(i >= input.size()) break;
-           while(input.at(i) == ' '){
+           while(input.at(i) == ' '){ //Test for whitespace
                 i++;
                 if(i >= input.size()) break;
            }
            if(input.at(i) == ';' || input.at(i) == '&' || 
               input.at(i) == '|'){
-              return false;
+              return false; 
            }
            else{
-              v.push_back(0);
+              v.push_back(0); //Returns value we set 0 as semicols
            }
         }
         else if(input.at(i) == '&'){
              if(i + 1 >= input.size()) break;
-             if(input.at(i+1) != '&'){
+             if(input.at(i+1) != '&'){ //Tests for &&
                 return false;
              }
              else{
@@ -101,7 +102,7 @@ bool getConnectors(const string &input, vector<int> & v){
                    return false;
                 }
                 else{
-                   v.push_back(1);
+                   v.push_back(1);//Returns 1 for &
                 }
              }
         }
@@ -122,7 +123,7 @@ bool getConnectors(const string &input, vector<int> & v){
                   return false;
                }
                else{
-                  v.push_back(2);
+                  v.push_back(2);//Returns 2 for ||
                }
              }
         } 
@@ -130,17 +131,17 @@ bool getConnectors(const string &input, vector<int> & v){
      return true;
 }
 
-void exec(string input, int& status){
+void exec(string input, int& status){ //Function that executes the commands
      //cout << "in exec" << endl;
      unsigned int i = 0;
-     char *argv[10000];
+     char *argv[10000]; //Set as huge size to take as many commands as user can write (hopefully)
      char *inputC;
      char *cmd;
      input.erase(input.find_last_not_of(" ") + 1);
      while(input.at(i) == ' '){
           i++;
      }
-     if(input.substr(i, 4) == "exit"){
+     if(input.substr(i, 4) == "exit"){ //Checks if exit is found by itself or in addition with other characters
           if(i + 4 < input.size()){
              i += 4;
           }
@@ -156,7 +157,7 @@ void exec(string input, int& status){
      i = 0;
      inputC = new char[input.size()];
      strcpy(inputC, input.c_str());
-     cmd = strtok(inputC, " \t");
+     cmd = strtok(inputC, " \t");//Parses based on spaces and tabs
      while(cmd != NULL){
          argv[i] = cmd;
          cmd = strtok(NULL, " \t");
@@ -168,7 +169,7 @@ void exec(string input, int& status){
         perror("Error with fork");
      }
      if(a == 0){
-        if(execvp(argv[0], argv) == -1){
+        if(execvp(argv[0], argv) == -1){ //Executes the command based on argument user types in 
             perror("Error with execvp");
             exit(-1);
         }
@@ -185,7 +186,7 @@ void exec(string input, int& status){
      }
 }
 
-void login(string &prompt){
+void login(string &prompt){ //Function created to get user login and hostname and print with prompt
    char host[512]; 
    if(getlogin() == NULL){
       perror("Error with getlogin()");
@@ -203,7 +204,7 @@ void login(string &prompt){
       }
    }
    else {
-      prompt = "$"; 
+      prompt = "$";  //If failed then output just a $
    }
    
 
@@ -214,21 +215,21 @@ int main(){
     bool operate = true;
     string prompt;
     login(prompt);
-    while(true){
+    while(true){ //Infinite loop for continuous commands until user types in exit
         cout << prompt << " ";
         vector<string> cmd;
         vector<int> cnct;
         cmd.clear();
         cnct.clear();
         string userIn;
-        getline(cin, userIn);
+        getline(cin, userIn);//Prompts user for input
         userIn.erase(userIn.find_last_not_of(" ") + 1);
         userIn.erase(0, userIn.find_first_not_of(" "));
         if(userIn.at(0) == ';' || userIn.at(0) == '|' || userIn.at(0) == '&'){
            cerr << "Error: beginning connectors" << endl;
         }
         else{
-           if(userIn.find("#") != string::npos){
+           if(userIn.find("#") != string::npos){ //Check for comments
               userIn.erase(userIn.find("#"), userIn.length());
            }
            operate = getConnectors(userIn, cnct);
